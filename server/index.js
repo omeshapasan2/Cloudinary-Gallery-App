8387ae69350e152aeed80096df873c63b985cc45
omeshapasan2 <omesha.20232170@iit.ac.lk>
2025-06-14 16:01:43 +0530

    Backend Endpoint Created

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cloudinary = require('cloudinary');
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
 
 // Middleware
 app.use(cors());
@@ -11,6 +16,41 @@ app.use(express.json());
 // Routes
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express!' });
+
+});
+
+app.get('/api/cloudinary', async (req, res) => {
+    
+    try{
+        const cloudName = process.env.CLOUD_NAME;
+
+        const response = await axios.get(
+            `https://api.cloudinary.com/v1_1/${cloudName}/resources/by_asset_folder`,
+            {
+                params: {
+                    asset_folder: 'React-Gallery-App',
+                },
+                auth: {
+                    username: process.env.CLOUDINARY_API_KEY,
+                    password: process.env.CLOUDINARY_API_SECRET
+                }
+            }
+        )
+
+        const cloudinaryData = response.data.resources.map(resource => ({
+                public_id: resource.public_id,
+                url: resource.secure_url,
+                format: resource.format,
+                width: resource.width,
+                height: resource.height
+            }));
+
+        res.json(cloudinaryData);
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch data from Cloudinary' });
+    }
 });
 
 // Start server
