7d2f2c3b5eef67492eded6810a274f77810d0a34
omeshapasan2 <omesha.20232170@iit.ac.lk>
2025-06-18 20:29:41 +0530

    Image Card Before Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {Routes , Route} from 'react-router-dom'
-import MainPage from './Pages/MainPage'
+// import MainPage from './Pages/MainPage'
 import { CloudinaryProvider } from './core/CloudinaryContext'
 import Home from './Pages/Home'
 import GalleryPage from './Pages/GalleryPage'
